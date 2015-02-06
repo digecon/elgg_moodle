11d8d7d0e3efc155294f2208cdd34ccf0b202511
Sergey Sachik <serega3000@gmail.com>
2015-02-06 06:56:57 +0300

    profile picture updater

--- lib/moodle.php
+++ lib/moodle.php
@@ -6,14 +6,31 @@ function moodle_update_profile_picture(ElggUser $user)
 	$moodleUser = $moodle->findUser($user->email);
 	if($moodleUser != null)
 	{
-		$icon = $user->getIconURL($size);
+		//http://kmstest.dig.center/mod/profile/icondirect.php?lastcache=1423189472&joindate=1419540682&guid=97&size=large
+		$moodle->query("core_user_update_users", array(
+			'users' => array(
+				array(
+					'id' => $moodleUser->id,
+					'preferences' => array(
+						'picture_url' =>
+						array(
+							'type' => 'picture_url',
+							'value' => $user->getIconURL('medium')
+						)
+					)					
+				)
+			)
+
+		));					
+		
+		/*$icon = $user->getIconURL($size);
 		if(strpos($icon, 'defaultlarge') === false)
 		{
 			$moodle->query("local_kms_update_profile_picture", array(
 				'email' => $user->email,
 				'picture' => file_get_contents($icon)
 			));			
-		}
+		}*/
 		/*
 		$icon = $user->getIconURL($size);
 		$contents = 		*/
