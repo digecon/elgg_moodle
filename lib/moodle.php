b2c3632a22bf42af462dbd80b57e4e45da954e8d
Sergey Sachik <serega3000@gmail.com>
2015-02-06 05:06:17 +0300

    user`s array bug

--- lib/moodle.php
+++ lib/moodle.php
@@ -26,14 +26,19 @@ function moodle_update_profile_data(ElggUser $user)
 	$moodleUser = $moodle->findUser($user->email);
 	if($moodleUser != null)
 	{
-		$moodle->query("core_user_update_users", array(array(
-			'id' => $moodleUser->id,
-			'city' => $user->location,
-			'description' => strip_tags($user->description),
-			'url' => elgg_normalize_url("profile/".$user->username),
-			'phone1' => $user->phone,
-			'phone2' => $user->mobile
-		)));
+		$moodle->query("core_user_update_users", array(
+			'users' => 
+				array(
+					array(
+						'id' => $moodleUser->id,
+						'city' => $user->location,
+						'description' => strip_tags($user->description),
+						'url' => elgg_normalize_url("profile/".$user->username),
+						'phone1' => $user->phone,
+						'phone2' => $user->mobile
+					)
+				)
+		));
 	}
 		
 }
