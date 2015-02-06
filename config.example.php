077df31a968c87fb83f68f977174bad17ac2e977
Sergey Sachik <serega3000@gmail.com>
2015-02-06 07:10:25 +0300

    added config

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+		'guest' => 9,
+		'member' => 10,
+		'observer' => 11,
+		'expert' => 12,
+		'curator' => 13,
+		'manager' => 14,
+		'moderator' => 15,
+		'admin' => 16	
+);
