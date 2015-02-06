b0410b8b1c226628b42a23d324d0126500e3da01
Sergey Sachik <serega3000@gmail.com>
2015-02-06 07:58:18 +0300

    update user`s info in LMS each login

--- start.php
+++ start.php
@@ -23,6 +23,15 @@ function moodle_profile_role_updated($event, $object_type, $user)
 	moodle_update_role($user);
 }
 
+function moodle_profile_logined($event, $object_type, $user)
+{
+	elgg_load_library('moodle:main');	
+	moodle_update_profile_data($user);
+	moodle_update_profile_picture($user);
+	moodle_update_role($user);
+}
+
+
 function moodle_init()
 {
 	//add a tab in site menu
@@ -38,3 +47,6 @@ elgg_register_event_handler('init', 'system', 'moodle_init');
 elgg_register_event_handler('profileupdate', 'user', 'moodle_profile_updated');
 elgg_register_event_handler('profileiconupdate', 'user', 'moodle_profile_icon_updated');
 elgg_register_event_handler('kmsroleupdate', 'user', 'moodle_profile_role_updated');
+elgg_register_event_handler('login:after', 'user', 'moodle_profile_logined');
+
+
