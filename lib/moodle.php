90311bd2c61aa66d52787ca81bb34f37336a9b25
Sergey Sachik <serega3000@gmail.com>
2015-02-06 05:03:52 +0300

    picture saving

--- lib/moodle.php
+++ lib/moodle.php
@@ -2,7 +2,22 @@
 
 function moodle_update_profile_picture(ElggUser $user)
 {
-	
+	$moodle = new Moodle();
+	$moodleUser = $moodle->findUser($user->email);
+	if($moodleUser != null)
+	{
+		$icon = $user->getIconURL($size);
+		if(strpos($icon, 'defaultlarge') === false)
+		{
+			$moodle->query("local_kms_update_profile_picture", array(
+				'email' => $user->email,
+				'picture' => file_get_contents($icon)
+			));			
+		}
+		/*
+		$icon = $user->getIconURL($size);
+		$contents = 		*/
+	}	
 }
 
 function moodle_update_profile_data(ElggUser $user)
@@ -14,8 +29,7 @@ function moodle_update_profile_data(ElggUser $user)
 		$moodle->query("core_user_update_users", array(array(
 			'id' => $moodleUser->id,
 			'city' => $user->location,
-			'description' => $user->description,
-			'descriptionformat' => 1,
+			'description' => strip_tags($user->description),
 			'url' => elgg_normalize_url("profile/".$user->username),
 			'phone1' => $user->phone,
 			'phone2' => $user->mobile
